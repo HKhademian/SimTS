4274db58fb5fff13e9d2c5b9921606b296786e12
Hossain <hossain.khademian@gmail.com>
2025-08-09 18:40:33 +0200

    check for X in gates

--- src/Gate.ts
+++ src/Gate.ts
@@ -6,42 +6,63 @@ export interface Gate {
 
 export class AndGate implements Gate {
     compute(inputs: Level[]): Level {
-        return inputs.every(l => l === Level.HIGH) ? Level.HIGH : Level.LOW;
+        if (inputs.includes(Level.X)) {
+            return Level.X;
+        }
+        return inputs.every(l => l === Level.H) ? Level.H : Level.L;
     }
 }
 
 export class OrGate implements Gate {
     compute(inputs: Level[]): Level {
-        return inputs.some(l => l === Level.HIGH) ? Level.HIGH : Level.LOW;
+        if (inputs.includes(Level.X)) {
+            return Level.X;
+        }
+        return inputs.some(l => l === Level.H) ? Level.H : Level.L;
     }
 }
 
 export class NotGate implements Gate {
     compute(inputs: Level[]): Level {
-        return inputs[0] === Level.HIGH ? Level.LOW : Level.HIGH;
+        if (inputs[0] === Level.X) {
+            return Level.X;
+        }
+        return inputs[0] === Level.H ? Level.L : Level.H;
     }
 }
 
 export class NandGate implements Gate {
     compute(inputs: Level[]): Level {
-        return inputs.every(l => l === Level.HIGH) ? Level.LOW : Level.HIGH;
+        if (inputs.includes(Level.X)) {
+            return Level.X;
+        }
+        return inputs.every(l => l === Level.H) ? Level.L : Level.H;
     }
 }
 
 export class NorGate implements Gate {
     compute(inputs: Level[]): Level {
-        return inputs.some(l => l === Level.HIGH) ? Level.LOW : Level.HIGH;
+        if (inputs.includes(Level.X)) {
+            return Level.X;
+        }
+        return inputs.some(l => l === Level.H) ? Level.L : Level.H;
     }
 }
 
 export class XorGate implements Gate {
     compute(inputs: Level[]): Level {
-        return inputs.filter(l => l === Level.HIGH).length % 2 === 1 ? Level.HIGH : Level.LOW;
+        if (inputs.includes(Level.X)) {
+            return Level.X;
+        }
+        return inputs.filter(l => l === Level.H).length % 2 === 1 ? Level.H : Level.L;
     }
 }
 
 export class XnorGate implements Gate {
     compute(inputs: Level[]): Level {
-        return inputs.filter(l => l === Level.HIGH).length % 2 === 0 ? Level.HIGH : Level.LOW;
+        if (inputs.includes(Level.X)) {
+            return Level.X;
+        }
+        return inputs.filter(l => l === Level.H).length % 2 === 0 ? Level.H : Level.L;
     }
 }
